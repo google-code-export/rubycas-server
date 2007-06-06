d66f48f667f36aba5074f2d64949e6db4cae2860
matt.zukowski <matt.zukowski@fffcb96a-a727-0410-ad3e-7b35c796b8d7>
2007-06-06 15:43:51 +0000

    wrong snapshot...

--- tags/releases/0.4.0/lib/casserver/authenticators/active_directory_ldap.rb
+++
@@ -1,8 +0,0 @@
-require 'casserver/authenticators/ldap'
-
-class CASServer::Authenticators::ActiveDirectoryLDAP < CASServer::Authenticators::LDAP
-  protected
-  def default_username_attribute
-    "sAMAccountName"
-  end
-end
